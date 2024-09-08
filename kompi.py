006b161337113623aacfc20f6d40682140eb4867
Eyzarim <96677601+Eyzarim@users.noreply.github.com>
2024-09-08 09:59:53 +0800

    ls

--- kompi.py
+++ kompi.py
@@ -1,9 +1,9 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# Load model dari safetensors (shards)
+# Load model dari safetensors shards
 model = AutoModelForCausalLM.from_pretrained("/app/tuned_model")
 tokenizer = AutoTokenizer.from_pretrained("/app/tuned_model")
 
-# Simpan model dalam format PyTorch (.bin)
-model.save_pretrained("/app/hasil-akhir/llama3_combined",max_shard_size="0GB")
-tokenizer.save_pretrained("/app/hasil-akhir/llama3_combined",max_shard_size="0GB")
+# Simpan model dalam satu file (tanpa sharding)
+model.save_pretrained("/app/hasil-akhir/llama3_combined", max_shard_size="0GB")
+tokenizer.save_pretrained("/app/hasil-akhir/llama3_combined")
