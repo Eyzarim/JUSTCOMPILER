c9cf67a9a095acae9e3f45e7f2c6d3cd32eb7e06
Eyzarim <96677601+Eyzarim@users.noreply.github.com>
2024-09-08 16:49:38 +0800

    new

--- merger.py
+++ merger.py
@@ -8,7 +8,7 @@ model = AutoModelForCausalLM.from_pretrained("/app/trainer/tuned_model", use_saf
 tokenizer = AutoTokenizer.from_pretrained("/app/trainer/tuned_model")
 
 # Simpan model dalam format PyTorch (.bin)
-model.save_pretrained("/app/hasil-akhir")
+model.save_pretrained("/app/hasil-akhir", max_shard_size="50GB")
 tokenizer.save_pretrained("/app/hasil-akhir")
 # Simpan model secara langsung menggunakan PyTorch tanpa sharding
 torch.save(model.state_dict(), "/app/hasil-akhir/pytorch_model_singlefile.bin")
